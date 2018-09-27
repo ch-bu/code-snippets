fdf419ef7375b5ad73790adcdd5cb48a950f39c8
Christian Burkhart <burkhart.christian@gmail.com>
2018-09-27 18:54:41 +0200

    feat: improve shiny app t-distribution

--- r/shiny/t-distribution/app.R
+++ r/shiny/t-distribution/app.R
@@ -1,38 +1,68 @@
 library(shiny)
-library(tidyverse)
+library(ggplot2)
 library(shinyWidgets)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("The Student T-Distribution Simulation"),
+   titlePanel("T-Distribution simulation"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("df1",
-                     "Degrees of Freedom 1:",
-                     min = 1,
-                     max = 50,
-                     value = 29),
-         
-         sliderInput("df2",
-                     "Degrees of Freedom 2:",
-                     min = 1,
-                     max = 50,
-                     value = 5),
-         materialSwitch(inputId = "show_plot_1", value = TRUE, 
-                        label = "Show first plot"),
-         materialSwitch(inputId = "show_plot_2", value = TRUE, 
-                        label = "Show first two"),
-         materialSwitch(inputId = "show_plot_3", value = TRUE, 
-                        label = "Show standard normal distribution")
+   fluidRow(
+     column(8, align = "center", 
+      plotOutput("densityPlot")
+    ),
+    column(3, 
+      HTML("<h2>About the simulation</h2>
+           <p>The t-distribution is a probability distribution, which is
+            widely used to test the difference between two sample means. 
+           The t-distribution
+           is commonly used for small sample sizes where the population
+           standard deviation is not none.</p>
+           <p>On the left you can see three distributions. The green and 
+           purple density plots display two t-distributions with 
+           arbitrary set degrees of freedom. The dashed line indicates 
+           the standard normal distribution. With an increase in the
+           degrees of freedom the t-distribution approximates the
+           standard normal distribution.<p>
+           <p>Try to adjust the degrees of freedom in both density plots
+           and see how the t-distributions behave compared
+           to the standard normal distribution.</p>")
+           )
+   ),
+   fluidRow(
+      column(4, 
+             HTML("<h2 style='color: #998ec3; text-align: center'>Purble T-Distribution</h2>"),
+             
+             column(8,
+                sliderInput("df1",
+                            "Adjust the degrees of freedom:",
+                            min = 1,
+                            max = 50,
+                            value = 29)
+                    ),
+              column(4,
+                 switchInput(inputId = "show_plot_1", value = TRUE, 
+                             label = "Show density distribution")
+                 )
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("densityPlot")
+      column(4,
+             HTML("<h2 style='color: #7fc97f; text-align: center'>Green T-Distribution</h2>"),
+             column(8,
+                sliderInput("df2",
+                            "Adjust the degrees of freedom:",
+                            min = 1,
+                            max = 50,
+                            value = 5)
+             ),
+             column(4,
+                    switchInput(inputId = "show_plot_2", value = TRUE, 
+                                label = "Show density distribution")
+             )
+      ),
+      column(1, 
+             switchInput(inputId = "show_plot_3", value = TRUE, 
+                         label = "Show standard normal distribution") 
       )
    )
 )
@@ -62,8 +92,8 @@ server <- function(input, output) {
                                       linetype = "dashed", alpha = .5)
       }
     
-      my_plot = my_plot + xlab("T-Werte") +
-        ylab("Dichte") +
+      my_plot = my_plot + xlab("x") +
+        ylab("P(x)") +
         xlim(-5, 5) +
         ylim(0, 0.5) +
         theme_minimal()
